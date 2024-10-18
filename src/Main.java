abc166281107e67c81541db0a730c34332475137
katana <klimenko.katrin888@mail.ru>
2024-10-18 09:45:32 +0300

    mili

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int ticket = 18500;
+        int percent = 20;
+
+        int bonus = (ticket / percent);
+
+        System.out.println("Бонусные мили" + bonus);
+
+    }
+}
+
